7d1b2101e08ce1a3f4c063e12160f913ad9b512a
Salah Eddine Zaka <zakasalaheddine@gmail.com>
2023-06-20 16:50:07 +0100

    feat: add google analytic to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import AppQueryClientProvider from '@/components/providers/query-client'
 import { Toaster } from '@/components/ui/toaster'
 import { Metadata } from 'next'
 import { querySEO } from '@/requests/query-seo'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,14 +21,30 @@ export async function generateMetadata(): Promise<Metadata> {
   }
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const siteSettings = await querySEO()
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${siteSettings?.googleAnalyticsId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${siteSettings?.googleAnalyticsId}');
+            `}
+          </Script>
+        </head>
         <body className={`${inter.className} min-h-screen`}>
           <AppQueryClientProvider>{children}</AppQueryClientProvider>
           <Toaster />
